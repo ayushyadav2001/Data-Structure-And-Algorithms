dc775f09f200ffc59ef5c8488a66678a47dcba49
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2021-05-05 02:35:39 +0530

    Add files via upload

---
+++ Arrays/subArrayVsSubSequences.cpp
@@ -0,0 +1,23 @@
+// sum of n subArray
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the size of array\n";
+     cin>>n;
+     int a[n];
+     for(int i=0;i<n;i++){
+         cin>>a[i];
+     }
+     int curr=0;
+     for(int i=0;i<n;i++){
+         curr=0;
+         for(int j=i;j<n;j++){
+             curr+=a[j];
+             cout<<curr<<endl;
+         }
+     }
+       
+        return 0;
+}
