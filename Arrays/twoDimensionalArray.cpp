1a55ac5d171a4ea6842ea22bf7f58848f6249f54
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2021-05-07 02:12:35 +0530

    Add files via upload

---
+++ Arrays/twoDimensionalArray.cpp
@@ -0,0 +1,36 @@
+// two dimensional array
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n, m;
+    cout << "Enter the dimensions of row";
+    cin >> n;
+    cout << "Enter the dimensions of col";
+    cin >> m;
+
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout<<"Matrrix is\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n ";
+    }
+
+    return 0;
+}
+// #ifndef ONLINE_JUDGE
+// freopen(_Filename: "input.txt",_Mode:"r",_file: stdin);
+// freopen(_Filename: "output.txt",_Mode:"w",_file: stdout);
+// #endif
